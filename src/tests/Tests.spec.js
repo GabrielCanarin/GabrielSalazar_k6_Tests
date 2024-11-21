a740e7f53d6520fc67abeab5208c73d1611f8de4
Gabriel Canarin Salazar <bielsalaz@hotmail.com>
2024-11-21 20:24:49 -0300

    00

---
+++ src/tests/Tests.spec.js
@@ -0,0 +1,51 @@
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import http from 'k6/http';
+import { check } from 'k6';
+import { Trend } from 'k6/metrics';
+
+export const getContactsDuration = new Trend('get_contacts', true);
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['avg<10000']
+  },
+  stages: [
+    { duration: '10s', target: 5 },
+    { duration: '20s', target: 10 },
+    { duration: '30s', target: 25 },
+    { duration: '50s', target: 50 },
+    { duration: '50s', target: 50 },
+    { duration: '30s', target: 25 },
+    { duration: '20s', target: 10 },
+    { duration: '10s', target: 5 },
+]
+};
+
+export function handleSummary(data) {
+  return {
+    './src/output/index.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true })
+  };
+}
+
+export default function () {
+  const baseUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const OK = 200;
+
+  const res = http.get(`${baseUrl}`, params);
+
+  getContactsDuration.add(res.timings.duration);
+
+  check(res, {
+    'GET Contacts - Status 200': () => res.status === OK
+  });
+}
